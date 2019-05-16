8551fd62addc1118a756c419a99e12088e0b25ac
w0000 <31149286+w0000@users.noreply.github.com>
2019-05-16 15:12:33 +0800

    Update and rename Hello.cpp to 王茂.cpp

--- 王茂.cpp
+++ 王茂.cpp
@@ -4,9 +4,12 @@
 #include<stdio.h>
 using namespace std;
 int main(){
-  cout << "1.优麒麟安全性能怎么样？";
-  cout << "2.优麒麟对于软件的兼容性还好吗？";
+  
+  cout << "1.优麒麟在这个云平台上使用比较卡。";
+  cout << "2.对于文字处理的软件可以着手考虑。";
+  
   cout << "Hello HENU." << endl;
   cout << "Hello Word." << endl;
+  
   return 0;
 }
